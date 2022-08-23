0ae3cd34f10f6a73d05f575ccda28e477ea66cc6
Pikachoc <104018295+Pikachoc@users.noreply.github.com>
2022-08-23 16:05:38 +0200

    Add files via upload

--- wp-config.php
+++ wp-config.php
@@ -22,21 +22,26 @@
 
 // ** Réglages MySQL - Votre hébergeur doit vous fournir ces informations. ** //
 /** Nom de la base de données de WordPress. */
-define('WP_CACHE', true);
-define( 'WPCACHEHOME', 'C:\wamp64\www\applicationvisitedelieux\wordpress\wp-content\plugins\wp-super-cache/' );
-define( 'DB_NAME', 'applicationvisitedelieux' );
+define( 'WPCACHEHOME', 'C:\wamp64\www\applicationvisitedelieux\wp-content\plugins\wp-super-cache/' );
+define( 'WP_CACHE', true );
+define( 'DB_NAME', "applicationvisitedelieux" );
+
 
 /** Utilisateur de la base de données MySQL. */
-define( 'DB_USER', 'root' );
+define( 'DB_USER', "root" );
+
 
 /** Mot de passe de la base de données MySQL. */
-define( 'DB_PASSWORD', '' );
+define( 'DB_PASSWORD', "" );
+
 
 /** Adresse de l’hébergement MySQL. */
-define( 'DB_HOST', 'localhost' );
+define( 'DB_HOST', "localhost" );
+
 
 /** Jeu de caractères à utiliser par la base de données lors de la création des tables. */
 define( 'DB_CHARSET', 'utf8mb4' );
+
 
 /**
  * Type de collation de la base de données.
@@ -56,13 +61,21 @@ define( 'DB_COLLATE', '' );
  * @since 2.6.0
  */
 define( 'AUTH_KEY',         ';;07QVFvuPg1S4/q4Qy:`feE(h>&]0(<F>AG^=pco2kh[& s[EyiI#Je%mh1&ZKB' );
+
 define( 'SECURE_AUTH_KEY',  'kazM-r1$?~9#:pv/.,fEQVf88=hJk4*.Z]7CO&)lLfv-9*w=nPxb@5&2UG7W@`]R' );
+
 define( 'LOGGED_IN_KEY',    'W%[ZA]#XFzU;XUYIp>L3jsDjD&k-}gTkFBzZ]l:L%Y3*0diR9#LHR[WA<*S$1:q&' );
+
 define( 'NONCE_KEY',        '1,1G{3KRv8Q1m6~l;uY:{ld.*w/=ab5$AdE)^wy4R:o]|S^hY[S$M1`[jwsu)r=i' );
+
 define( 'AUTH_SALT',        ':r{k5!a^6Je|kOX3bTBU`ztLI1Kq^*[c~6?%+LKP=omC]Ik/q5ZS/o%z[sc;5uzR' );
+
 define( 'SECURE_AUTH_SALT', 'rM?O:lS5Bg#dwS5p2V$?<[:Q T<juiM{/CG>-d^s.Us8im5u}s/Ku)-ZOy83d|,F' );
+
 define( 'LOGGED_IN_SALT',   '4rDtusX{!9)O}k9`Q@M1]@bZ~2)c|k9YciII!</=Y2=0*1a&G*K6,Yp4k;!o1ZS}' );
+
 define( 'NONCE_SALT',       'DBf?dSOMWIcgr@N@VpF(2[bi<q1i[j^)u(LTsh/<zB^PD!>kxC!J}O;[O*%Zjv|H' );
+
 /**#@-*/
 
 /**
@@ -73,6 +86,7 @@ define( 'NONCE_SALT',       'DBf?dSOMWIcgr@N@VpF(2[bi<q1i[j^)u(LTsh/<zB^PD!>kxC!
  * N’utilisez que des chiffres, des lettres non-accentuées, et des caractères soulignés !
  */
 $table_prefix = 'wp_';
+
 
 /**
  * Pour les développeurs : le mode déboguage de WordPress.
